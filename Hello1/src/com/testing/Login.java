2cedd8403887f53b736e8d2272ab3f4f8370287b
SunnyG <sunny.gao2009@gmail.com>
2016-12-11 13:00:49 -0500

    tess

--- Hello1/src/com/testing/Login.java
+++ Hello1/src/com/testing/Login.java
@@ -5,6 +5,8 @@ public class Login {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println ("Login 1___");
+		
+		System.out.println ("Login 1___");
 
 	}
 
